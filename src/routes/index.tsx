a93c7e2e5017908e25482309cac31a2375ef78d2
stuartjames7845 <142866649+stuartjames7845@users.noreply.github.com>
2023-08-22 14:02:41 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,10 +8,10 @@ export default function Home() {
       <h1>CloudBik Softwares</h1>
       <Counter />
       <p>
-        Modern, efficient, reliable - that's Cloudbik! Our state-of-the-art SaaS platform has been designed to help you seamlessly migrate your organization's data between different digital workplaces. Whether you're moving from Microsoft 365 to Google Workspace, Zoho Workplace to Amazon Work Mail, etc.<br>
-Our team of experts understands that every organization is unique – that’s why we offer customized solutions to fit your specific needs. Our comprehensive suite of services includes planning, migration strategy development, implementation support, ongoing maintenance, and monitoring.<br>
-At Cloudbik, we pride ourselves on our commitment to customer satisfaction. We work closely with our clients from start to finish ensuring they are fully informed throughout the entire process.<br>
-Whether you’re looking to migrate your email system or simply looking for advice on how best to optimize it in the cloud - Cloudbik has got you covered! Contact us today for more information about how we can help transform your organization’s email system.<br>
+        Modern, efficient, reliable - that's Cloudbik! Our state-of-the-art SaaS platform has been designed to help you seamlessly migrate your organization's data between different digital workplaces. Whether you're moving from Microsoft 365 to Google Workspace, Zoho Workplace to Amazon Work Mail, etc.
+Our team of experts understands that every organization is unique – that’s why we offer customized solutions to fit your specific needs. Our comprehensive suite of services includes planning, migration strategy development, implementation support, ongoing maintenance, and monitoring.
+At Cloudbik, we pride ourselves on our commitment to customer satisfaction. We work closely with our clients from start to finish ensuring they are fully informed throughout the entire process.
+Whether you’re looking to migrate your email system or simply looking for advice on how best to optimize it in the cloud - Cloudbik has got you covered! Contact us today for more information about how we can help transform your organization’s email system.
         Visit us at: <a href=https://www.cloudbik.com>www.cloudbik.com </a>
       </p>
     </main>
