c32f4e976d7964b6fa15ea45e14180b05dbbcbf7
stuartjames7845 <142866649+stuartjames7845@users.noreply.github.com>
2023-08-22 13:35:07 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
+      <Title>CloudBik Softwares</Title>
       <h1>CloudBik Softwares</h1>
       <Counter />
       <p>
